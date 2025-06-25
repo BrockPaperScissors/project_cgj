b1fe60b8bd773514f176790575dade3ce0b5fefe
BrockPaperScissors <michaeljbrock91@gmail.com>
2025-06-24 20:37:50 -0700

    player and fridge inventory skeleton laid out, fridge animation implemented

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,39 +2,47 @@ extends Node2D
 
 @onready var main_menu: Control = $"../MainMenu"
 @onready var player = $"../Player"
-@onready var ui = $"../UiManager"
-#@onready var player_inventory = ui.get_node("PlayerInventory")
+#@onready var ui_inventory = $"../UiManager/PlayerInventoryWindow"
+@onready var player_inventory: PanelContainer = $"../CanvasLayer/playerInventory"
+@onready var fridge_inventory: PanelContainer = $"../CanvasLayer/fridgeInventory"
+@onready var fridge_animation: AnimatedSprite2D = $"../Fridge/AnimatedSprite2D"
+@onready var fridge: StaticBody2D = $"../Fridge"
+const SEED_1 = preload("res://Assets/polishedAssets/seed_1.png")
 
+@export var item_list = [{"name": 'Carrots', "icon": preload("res://Assets/UI/icons/plant1.png")}]
 
 var carrotQuant : int = 0
-	
+
 func _process(delta):
 	if Input.is_action_just_pressed("open_main_menu"):
-		main_menu.visible = not main_menu.visible
-		if main_menu.visible == true:
-			ui.visible = false
-			player.visible = false
-		else:
-			ui.visible = true
-			player.visible = true
-		
-	#if Input.is_action_just_pressed("open_player_inventory"):
-		#if not ui.get_node("ExternalInventory").visible and not main_menu.visible:
-			#player_inventory.position = getPlayerPos()
-			#player_inventory.visible = not player_inventory.visible
+		toggleMainMenu()
+	
+	if Input.is_action_just_pressed("open_player_inventory"):
+		toggleInventory(player_inventory)
 			
 
 func _on_flower_pot_plant_gathered(quantity: Variant, type: Variant) -> void:
 	print("Adding: ", quantity, " ", type)
 	carrotQuant = carrotQuant + quantity
 	print(str(carrotQuant) + " Carrots")
+	fridge_inventory.add_item('carrot', SEED_1)
 	
-func toggleInventory():
-	#player_inventory.position = getPlayerPos()
-	#player_inventory.visible = not player_inventory.visible
-	pass
-
+func toggleInventory(inventory : PanelContainer):
+	#canvas_layer.position = getPlayerPos()
+	inventory.visible = !(inventory.visible)
+	
+func toggleMainMenu():
+	main_menu.visible = !(main_menu.visible)
 	
 func getPlayerPos():
 	var playerPos = player.position
 	return playerPos
+
+
+func _on_fridge_fridge_opened() -> void:
+	fridge_inventory.position = fridge.position
+	toggleInventory(fridge_inventory)
+
+func _on_fridge_fridge_closed() -> void:
+	#toggleInventory(fridge_inventory)
+	pass
